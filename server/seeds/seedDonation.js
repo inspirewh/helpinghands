fbd1f3254918b981719edc01f6b92b2a5b4408bb
Jake Roberts <jake.shaun.roberts1994@hotmail.com>
2022-10-18 20:05:24 +0800

    added more seeds for user and donations

--- server/seeds/seedDonation.js
+++ server/seeds/seedDonation.js
@@ -63,6 +63,140 @@ const donations = [
     // boolean
     item_status_sent: true,
   },
+  {
+    // string
+    item_name: "SeedTestingNo4",
+
+    // string
+    item_description: "funny dog",
+
+    // boolean
+    item_recieved: true,
+
+    // string
+    item_imageUrl: "https://api.time.com/wp-content/uploads/2014/06/450965060.jpg",
+
+    // number
+    item_quantity: 1,
+    
+    // boolean
+    item_status_sent: true,
+  },
+  {
+    // string
+    item_name: "seededItemNumber5",
+
+    // string
+    item_description: "delicious fish",
+
+    // boolean
+    item_recieved: false,
+
+    // string
+    item_imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/2006_sardines_can_open.jpg/220px-2006_sardines_can_open.jpg",
+
+    // number
+    item_quantity: 2,
+    
+    // boolean
+    item_status_sent: true,
+  },
+  {
+    // string
+    item_name: "seededDonation6",
+
+    // string
+    item_description: "patriotic sweater",
+
+    // boolean
+    item_recieved: true,
+
+    // string
+    item_imageUrl: "https://media.cnn.com/api/v1/images/stellar/prod/191211145618-02-ugly-christmas-sweater-1211-trump.jpg?q=x_2,y_166,h_1123,w_1995,c_crop/h_270,w_480",
+
+    // number
+    item_quantity: 7,
+    
+    // boolean
+    item_status_sent: true,
+  },
+  {
+    // string
+    item_name: "seedNumber7",
+
+    // string
+    item_description: "warm socks",
+
+    // boolean
+    item_recieved: true,
+
+    // string
+    item_imageUrl: "https://i.etsystatic.com/6572991/r/il/b91cff/3593874136/il_570xN.3593874136_qf0e.jpg",
+
+    // number
+    item_quantity: 2000,
+    
+    // boolean
+    item_status_sent: true,
+  },
+  {
+    // string
+    item_name: "seedSocks8",
+
+    // string
+    item_description: "fun toy for kids",
+
+    // boolean
+    item_recieved: false,
+
+    // string
+    item_imageUrl: "https://i.ebayimg.com/images/g/1q4AAOSwC0dbtnBx/s-l500.jpg",
+
+    // number
+    item_quantity: 1,
+    
+    // boolean
+    item_status_sent: true,
+  },
+  {
+    // string
+    item_name: "seedySeed9",
+
+    // string
+    item_description: "1 cheese block, still in date, tastes good",
+
+    // boolean
+    item_recieved: true,
+
+    // string
+    item_imageUrl: "https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/uqbmfey72kvyyux2b50d.jpg",
+
+    // number
+    item_quantity: 1,
+    
+    // boolean
+    item_status_sent: true,
+  },
+  {
+    // string
+    item_name: "seedME10",
+
+    // string
+    item_description: "1x unconcious human",
+
+    // boolean
+    item_recieved: false,
+
+    // string
+    item_imageUrl: "https://www.firstaidpro.com.au/wp-content/uploads/2020/11/X0000_Physio_LifePak-CR-Plus-at-EMS-handoff-copy.jpg",
+
+    // number
+    item_quantity: 1,
+    
+    // boolean
+    item_status_sent: true,
+  }
+  
 ]
 
 async function seedDonation(){
@@ -71,64 +205,28 @@ async function seedDonation(){
   return await Donation.insertMany(donations);
 
   //connect mongoose
-  mongoose
-    .connect(String(connection.db), { useNewUrlParser: true })
-    .catch(err => {
-      console.log(err.stack);
-      process.exit(1);
-    })
-    .then(() => {
-      console.log("connected to db in development environment");
-    });
-  //save your data. this is an async operation
-  //after you make sure you seeded all the products, disconnect automatically
-  donations.map(async (don, index) => {
-    const model = new Donation(don);
-    await model.save((err, result) => {
+  // mongoose
+  //   .connect(String(connection.db), { useNewUrlParser: true })
+  //   .catch(err => {
+  //     console.log(err.stack);
+  //     process.exit(1);
+  //   })
+  //   .then(() => {
+  //     console.log("connected to db in development environment");
+  //   });
+  // //save your data. this is an async operation
+  // //after you make sure you seeded all the products, disconnect automatically
+  // donations.map(async (don, index) => {
+  //   const model = new Donation(don);
+  //   await model.save((err, result) => {
   
       
-      if (index === donations.length - 1) {
-        console.log("DONE!");
-        mongoose.disconnect();
-      }
-    });
-  });
+  //     if (index === donations.length - 1) {
+  //       console.log("DONE!");
+  //       mongoose.disconnect();
+  //     }
+  //   });
+  // });
 };
 
 module.exports = seedDonation;
-
-
-//TODO: below is useful pseudo code
-// // grab a random user
-// User.findOneAndUpdate()
-// // add model._id
-
-// // $addToSet
-// module.exports = function seedDonations(){
-//   // +.......
-
-//   return seededDonations
-// }
-
-
-
-// // // index.js in seeds folder
-// // const users = seedUsers();
-
-// // const dons = seedDonations();
-
-// // for (let index = 0; index < dons.length; index++) {
-// //   const don = dons[index];
-  
-
-// //   // get a random user from 'users'
-// //   const randomUser = ....
-
-
-
-// //   User.findOneByIdAndUpdate(randomUser._id, {
-// //     $addToSet: {
-// //       donations_ids: don._id
-// //     }
-// //   })
-// // }
--- server/seeds/seedUser.js
+++ server/seeds/seedUser.js
@@ -28,13 +28,90 @@ const users = [
   },
   {
     // string
-    username: "testingUserSeed1",
+    username: "testingUserSeed3",
 
     // string
-    email: "testingUserSeed1@gmail.com",
+    email: "testingUserSeed3@gmail.com",
 
     // boolean
     password: "hopethisworks123",
+
+  },
+  {
+    // string
+    username: "userTest444",
+
+    // string
+    email: "testICLE444@gmail.com",
+
+    // boolean
+    password: "pleasework444",
+
+  },
+  {
+    // string
+    username: "55test55",
+
+    // string
+    email: "55test55@gmail.com",
+
+    // boolean
+    password: "5noerrors5",
+
+  },
+  {
+    // string
+    username: "666devils666",
+
+    // string
+    email: "satan666@gmail.com",
+
+    // boolean
+    password: "666lucifer666",
+
+  },
+  {
+    // string
+    username: "7heaven7",
+
+    // string
+    email: "heavenlysevenly7@gmail.com",
+
+    // boolean
+    password: "7minutesin7",
+
+  },
+  {
+    // string
+    username: "8yamate8",
+
+    // string
+    email: "jeffreydahmer8yourmama@gmail.com",
+
+    // boolean
+    password: "humankebabs8",
+
+  },
+  {
+    // string
+    username: "finelinebetween9",
+
+    // string
+    email: "lookinfinenumber9@gmail.com",
+
+    // boolean
+    password: "sixty9",
+
+  },
+  {
+    // string
+    username: "lookagainnumber10",
+
+    // string
+    email: "tricking10sdroppingbens@gmail.com",
+
+    // boolean
+    password: "10isthepassword",
 
   }
 ]
@@ -44,28 +121,26 @@ async function seedUsers(){
   
 
   return await User.insertMany(users);
-  mongoose
-    .connect(String(connection.db), { useNewUrlParser: true })
-    .catch(err => {
-      console.log(err.stack);
-      process.exit(1);
-    })
-    .then(() => {
-      console.log("connected to db in development environment");
-      //save your data. this is an async operation
-      //after you make sure you seeded the User, disconnect automatically
-      users.map(async (p, index) => {
-        await p.save((err, result) => {
-          if (index === users.length - 1) {
-            console.log("DONE!");
-            mongoose.disconnect();
-          }
-        });
-      });
-    });
-  // const userSeeds = users;
-  
   //connect mongoose
-}
+  // mongoose
+  //   .connect(String(connection.db), { useNewUrlParser: true })
+  //   .catch(err => {
+  //     console.log(err.stack);
+  //     process.exit(1);
+  //   })
+  //   .then(() => {
+  //     console.log("connected to db in development environment");
+  //     //save your data. this is an async operation
+  //     //after you make sure you seeded the User, disconnect automatically
+  //     users.map(async (p, index) => {
+  //       await p.save((err, result) => {
+  //         if (index === users.length - 1) {
+  //           console.log("DONE!");
+  //           mongoose.disconnect();
+  //         }
+  //       });
+  //     });
+  //   });
+};
 
 module.exports = seedUsers;
