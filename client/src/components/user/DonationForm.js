f509b2ab2c68698529d1f2ad14a7b05dd3ad61e4
inspirewh <78182677+inspirewh@users.noreply.github.com>
2022-10-20 20:38:35 +0800

    donation form

--- client/src/components/user/DonationForm.js
+++ client/src/components/user/DonationForm.js
@@ -1,4 +1,5 @@
 import { useState }from "react";
+import { Link } from 'react-router-dom';
 import { Col, Container, Row } from "react-bootstrap";
 import { useMutation } from '@apollo/client';
 import { ADD_DONATION } from '../../utils/mutations';
@@ -25,40 +26,66 @@ export const Donate = ({ profileId }) => {
     
     return (
         <section className="donationForm" id="donate">
-        {auth.loggedIn() ? (
             <Container>
                 <Row className="justify-content-center align-items-center" id= "inputForm">
                     <Col className="inputForm" md={6}>
                         <h2>Let's Get Donating</h2>
                         <p>Want to know more about Helping Hands. Or want to get involved more and support those in need? Get in touch by completing the form below and we'll be in touch.</p>
-                        <form>
+                        {auth.loggedIn() ? (
+                        <form onSubmit={handleFormSubmit} >
                             <Row>
                                 <Col>
-                                <input id="imageUpload" type= 'file' value={formDetails.imgupload} placeholder= "Add photos of your donation" onChange={(e) => onFormUpdate('imgUpload', e.target.value)}></input>
+                                <input 
+                                placeholder= "Add photos of your donation"
+                                type= 'file'
+                                name="itemImage"
+                                value={donation.itemImage}                                 
+                                onChange={(event) => setDonation(event.target.value)}></input>
                                 </Col>
                             </Row>
                             <Row> 
                                 <Col sm={6} className="px-1">
-                                    <input type="text" value={formDetails.title} placeholder= "Title" onChange={(e) => onFormUpdate('title', e.target.value)} />
+                                <input 
+                                placeholder= "Title" 
+                                type= 'text'
+                                name="itemTitle"
+                                value={donation.itemTitle}                                 
+                                onChange={(event) => setDonation(event.target.value)}></input>
                                 </Col>
                             </Row>
                             <Row>
                                 <Col>
-                                    <textarea row="6" value={formDetails.message} placeholder= "Description" onChange={(e) => onFormUpdate('description', e.target.value)}/>
-                                    <button type="submit"><span>{buttontext}</span></button>
+                                <input 
+                                placeholder= "Description" 
+                                type= 'text'
+                                name="itemDescription"
+                                value={donation.itemDescription}                                 
+                                onChange={(event) => setDonation(event.target.value)}></input>
+                                <button type="submit"></button>
+                                {error && (
+                                    <div className="col-12 my-3 bg-danger text-white p-3">
+                                    {error.message}
+                                    </div>
+                                 )}
                                 </Col>
                             </Row>
+                            {error && (
+                            <div className="col-12 my-3 bg-danger text-white p-3">
+                            {error.message}
+                            </div>
+                        )}
                         </form>
+                        ) : (
+                        <p>
+                        You need to be logged in to donate Please{' '}
+                        <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
+                        </p>
+                        )}
                     </Col>
                 </Row>
             </Container>
-                ) : (
-                    <p>
-                    You need to be logged in to donate. Please{' '}
-                    <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
-                    </p>
-                )}
-                </div>
-            );
-            };
+
         </section>
+
+    );
+};
